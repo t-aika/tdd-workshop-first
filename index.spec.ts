e89962e17d3d6e5be3288d03163c1ca5b437f64e
tomatsu.aika <tomatsu.aika@fujitsu.com>
2024-11-29 16:31:07 +0900

    initial commit

---
+++ index.spec.ts
@@ -0,0 +1,9 @@
+import { rsp } from "./index"
+
+it('true is true', () => {
+  expect(true).toEqual(false);
+});
+
+// it('p1 rock vs p2 paper is p2wins', () => {
+//   expect(rsp('rock', 'paper')).toEqual('p2wins');
+// });
---
+++ index.ts
@@ -0,0 +1,7 @@
+export type Hand = 'rock' | 'paper' | 'scissors'
+export type GameResult = 'p1wins' | 'p2wins' | 'tie'
+
+export const rsp
+  = (p1Hand: Hand, p2Hand: Hand): GameResult => {
+    return ''
+  }
